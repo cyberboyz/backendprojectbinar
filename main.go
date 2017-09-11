0d0a696a4966ebc36b34741f8e7ec95f81c6a750
Fattah Azzuhry <fattahazzuhry@gmail.com>
2017-09-12 00:51:39 +0700

    edit main.go

--- main.go
+++ main.go
@@ -86,7 +86,9 @@ var db *gorm.DB
 var err error
 
 func main() {
-	db, err = gorm.Open("postgres", "host=localhost user=postgres dbname=gorm sslmode=disable password=postgres")
+	// db, err = gorm.Open("postgres", "host=localhost user=postgres dbname=gorm sslmode=disable password=postgres")
+	db, err = gorm.Open("postgres", "postgres://ssvuoibpdkugsp:c73e9d4dfbc63b197d4c2336c77adcc4d974afccf584f8a5e793d1e7ac90d242@ec2-50-17-236-15.compute-1.amazonaws.com:5432/dso2rra5vg6aa")
+
 	db.SingularTable(true)
 	if err != nil {
 		fmt.Println(err)
