9688aeec871bb72e3273a41391a3b18803c58608
Fattah Azzuhry <fattahazzuhry@gmail.com>
2017-09-20 14:28:20 +0700

    ngedit lagi

--- main.go
+++ main.go
@@ -154,6 +154,13 @@ func main() {
 
 	router := gin.New()
 
+	router.LoadHTMLGlob("htmlfile/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+
 	v1 := router.Group("/v1")
 	{
 
